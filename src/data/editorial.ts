1bb7c09c79de440ba4a1cb862b0f67383924c550
Kunal Pundalik Mahajan <Inkunalmahajan@gmail.com>
2026-09-03 22:50:22 +0530

    Feature IBPS RRB XV update on homepage

--- src/data/editorial.ts
+++ src/data/editorial.ts
@@ -1,14 +1,14 @@
 export const homepageEditorial = {
   leadStoryId: 'india-post-gds-recruitment-2026-deaf-hard-of-hearing-eligible',
   sideStoryIds: [
+    'ibps-rrb-recruitment-2026-hi-deaf-hard-of-hearing',
     'upsc-epfo-assistant-provident-fund-commissioner-recruitment-2026',
-    'kurnool-pwd-special-recruitment-drive-hh-final-merit-lists-2026',
   ],
   breakingStoryId: 'india-post-gds-recruitment-2026-deaf-hard-of-hearing-eligible',
   pinnedTrendingIds: [
     'india-post-gds-recruitment-2026-deaf-hard-of-hearing-eligible',
+    'ibps-rrb-recruitment-2026-hi-deaf-hard-of-hearing',
     'upsc-epfo-assistant-provident-fund-commissioner-recruitment-2026',
-    'kurnool-pwd-special-recruitment-drive-hh-final-merit-lists-2026',
   ],
   latestCount: 4,
   trendingCount: 5,
